3c5718ef34d96dade9407b59731de71407ffdd6f
wundzunhome@wp.pl <wundzunhome@wp.pl@c213c24a-fd2c-11dd-a7be-4b3ef3b0700c>
2009-02-21 13:18:30 +0000

    Generowanie tekstu do ćwiczeń przy pomocy łańcucha Markowa.

---
+++ src/coolkey/TextfileFilter.java
@@ -0,0 +1,13 @@
+package coolkey;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+/**
+ * Filtr sprawdzający, czy podane pliki mają rozszerzenie txt.
+ */
+public class TextfileFilter implements FilenameFilter {
+	public boolean accept(File dir, String name) {
+		return name.toLowerCase().endsWith(".txt");
+	}
+}
