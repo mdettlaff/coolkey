6cb44c9707ffb814f3494c491442c3766beb172d
wundzunhome@wp.pl <wundzunhome@wp.pl@c213c24a-fd2c-11dd-a7be-4b3ef3b0700c>
2009-04-15 19:06:51 +0000

    Przepisywanie: przewijanie tekstu.

--- src/coolkey/gui/WritingArea.java
+++ src/coolkey/gui/WritingArea.java
@@ -18,6 +18,14 @@ import coolkey.CoolKey;
  * Obszar na którym odbywa się przepisywanie.
  */
 public class WritingArea {
+	private final int MAX_LINES = 10;
+	/**
+	 * Maksymalna ilość przepisanych linii jaką widać na ekranie.
+	 */
+	private final int MAX_TYPING_LINES = 4;
+	private final int LEFT_MARGIN = 15;
+	private final int TOP_MARGIN_TEXT = 6;
+	private final int TOP_MARGIN_WRITTEN = 27;
 	private final int LINE_HEIGHT = 44;
 
 	private Canvas writingArea;
@@ -30,15 +38,21 @@ public class WritingArea {
 
 			@Override
 			public void keyPressed(KeyEvent keyEvent) {
-				char c = keyEvent.character;
-				if (c == '\r') {
-					CoolKey.getCurrentLesson().typeEnter();
-				} else if (c == SWT.BS) {
-					CoolKey.getCurrentLesson().typeBackspace();
-				} else if (!Character.isISOControl(c)) {
-					CoolKey.getCurrentLesson().typeChar(c);
+				if (!CoolKey.getCurrentLesson().isFinished()) {
+					char c = keyEvent.character;
+					if (c == '\r') {
+						CoolKey.getCurrentLesson().typeEnter();
+						if (CoolKey.getCurrentLesson().isFinished()) {
+							// TODO: Okienko z wynikami.
+							System.out.println("Pokaż wyniki");
+						}
+					} else if (c == SWT.BS) {
+						CoolKey.getCurrentLesson().typeBackspace();
+					} else if (!Character.isISOControl(c)) {
+						CoolKey.getCurrentLesson().typeChar(c);
+					}
+					writingArea.redraw();
 				}
-				writingArea.redraw();
 			}
 
 			public void keyReleased(KeyEvent keyEvent) {}
@@ -51,17 +65,34 @@ public class WritingArea {
 				gc.setFont(new Font(GUI.display, "Courier New", 10, SWT.NORMAL));
 				Point areaSize = writingArea.getSize();
 				gc.fillRectangle(0, 0, areaSize.x, areaSize.y);
+				// zakres linii do wyświetlenia
+				int startLine = CoolKey.getCurrentLesson().
+						getWrittenLines().size() - MAX_TYPING_LINES;
+				if (startLine < 0) {
+					startLine = 0;
+				}
+				int endLine = startLine + MAX_TYPING_LINES - 1;
+				if (endLine > CoolKey.getCurrentLesson().getWrittenLines().size() - 1) {
+					endLine = CoolKey.getCurrentLesson().getWrittenLines().size() - 1;
+				}
 				// narysuj tekst do przepisania
-				int x = 15;
-				int y = 8;
-				for (String line : CoolKey.getCurrentLesson().getTextLines()) {
+				int x = LEFT_MARGIN;
+				int y = TOP_MARGIN_TEXT;
+				for (int i = startLine; i <= endLine; i++) {
+					String line = CoolKey.getCurrentLesson().getTextLines().get(i);
 					gc.drawString(line, x, y);
 					y += LINE_HEIGHT;
 				}
+				for (int i = endLine + 1; i < CoolKey.getCurrentLesson().getTextLines().size()
+						&& i < endLine + 1 + MAX_LINES - 2 * (1 + endLine - startLine); i++) {
+					String line = CoolKey.getCurrentLesson().getTextLines().get(i);
+					gc.drawString(line, x, y);
+					y += LINE_HEIGHT / 2;
+				}
 				// narysuj przepisany tekst
 				gc.setForeground(GUI.display.getSystemColor(SWT.COLOR_BLUE));
-				y = 28;
-				for (int i=0; i < CoolKey.getCurrentLesson().getWrittenLines().size(); i++) {
+				y = TOP_MARGIN_WRITTEN;
+				for (int i = startLine; i <= endLine; i++) {
 					String line = CoolKey.getCurrentLesson().getWrittenLines().get(i);
 					gc.drawString(line, x, y);
 					y += LINE_HEIGHT;
@@ -81,15 +112,17 @@ public class WritingArea {
 				gc.drawString(cursor, x, y, true);
 				// zaznacz znaki które zostały poprawione
 				gc.setForeground(new Color(GUI.display, 192, 0, 192));
-				y = 28;
-				for (String line : CoolKey.getCurrentLesson().getCorrections()) {
+				y = TOP_MARGIN_WRITTEN;
+				for (int i = startLine; i <= endLine; i++) {
+					String line = CoolKey.getCurrentLesson().getCorrections().get(i);
 					gc.drawString(line, x, y, true);
 					y += LINE_HEIGHT;
 				}
 				// zaznacz pomyłki na czerwono
 				gc.setForeground(GUI.display.getSystemColor(SWT.COLOR_RED));
-				y = 28;
-				for (String line : CoolKey.getCurrentLesson().getMistakes()) {
+				y = TOP_MARGIN_WRITTEN;
+				for (int i = startLine; i <= endLine; i++) {
+					String line = CoolKey.getCurrentLesson().getMistakes().get(i);
 					gc.drawString(line, x, y, true);
 					y += LINE_HEIGHT;
 				}
