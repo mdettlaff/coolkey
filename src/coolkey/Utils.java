bd4f7e9a4d6b4501e947f470cb833c1001c10567
wundzunhome@wp.pl <wundzunhome@wp.pl@c213c24a-fd2c-11dd-a7be-4b3ef3b0700c>
2009-04-18 13:32:40 +0000

    Nie powinno już pokazywać krzaków, jeśli w systemie jest ustawione kodowanie inne niż UTF-8. Dodałem też możliwość ćwiczenia z użyciem własnych plików tekstowych.

--- src/coolkey/Utils.java
+++ src/coolkey/Utils.java
@@ -2,8 +2,10 @@ package coolkey;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,8 +24,9 @@ public class Utils {
 		List<String> corpus = new ArrayList<String>();
 		try {
 			for (File file : files) {
-				for (String line : Utils.readFileAsString(file).replaceAll(" +", " ").
-						replace(")", "").replace("(", "").replace("\"", "").
+				for (String line : Utils.readFileAsString(file, "UTF-8").
+						replaceAll(" +", " ").replace(")", "").
+						replace("(", "").replace("\"", "").
 						trim().split("\n")) {
 					for (String word : line.split(" ")) {
 						corpus.add(word.trim());
@@ -37,16 +40,38 @@ public class Utils {
 	}
 
 	/**
-	 * Czyta treść pliku i zwraca ją jako napis.
+	 * Czyta treść pliku i zwraca ją jako napis. Przy odczytywaniu pliku
+	 * stosowane jest kodowanie znaków domyślne dla danej platrormy.
 	 *
-	 * @param file Plik który chcemy odczytać.
-	 * @return     Zawartość pliku jako napis.
+	 * @param  file Plik który chcemy odczytać.
+	 * @return      Zawartość pliku jako napis.
 	 */
 	public static String readFileAsString(File file)
 		throws IOException {
 		StringBuffer fileData = new StringBuffer(1000);
 		BufferedReader reader = new BufferedReader(
 				new FileReader(file));
+		char[] buf = new char[1024];
+		int numRead=0;
+		while((numRead = reader.read(buf)) != -1){
+			fileData.append(buf, 0, numRead);
+		}
+		reader.close();
+		return fileData.toString();
+	}
+
+	/**
+	 * Czyta treść pliku i zwraca ją jako napis.
+	 *
+	 * @param  file     Plik który chcemy odczytać.
+	 * @param  encoding Nazwa kodowania znaków w odczytywanym pliku.
+	 * @return          Zawartość pliku jako napis.
+	 */
+	public static String readFileAsString(File file, String encoding)
+		throws IOException {
+		StringBuffer fileData = new StringBuffer(1000);
+		BufferedReader reader = new BufferedReader(
+				new InputStreamReader(new FileInputStream(file), encoding));
 		char[] buf = new char[1024];
 		int numRead=0;
 		while((numRead = reader.read(buf)) != -1){
