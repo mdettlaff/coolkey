7015f5acda34c77b9379a8a1dd39e54a9ff326d6
wundzunhome@wp.pl <wundzunhome@wp.pl@c213c24a-fd2c-11dd-a7be-4b3ef3b0700c>
2009-04-26 13:00:27 +0000

    Naprawiłem Issue 2.

--- trunk/src/coolkey/Sound.java
+++ trunk/src/coolkey/Sound.java
@@ -1,106 +1,72 @@
 package coolkey;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.SourceDataLine;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
+/**
+ * Efekty dźwiękowe podczas przepisywania.
+ */
 public class Sound {
-	private static final int EXTERNAL_BUFFER_SIZE = 1000;
-	private static final String TYPING_SOUND_FILENAME = "data/sound/type.wav";
-	private static final String MISTAKE_SOUND_FILENAME = "data/sound/mistake.wav";
+	private static final String SND_PATH = "data" + File.separator
+			+ "sound" + File.separator;
+	private static final String TYPING_SOUND_PATH = SND_PATH + "type.wav";
+	private static final String MISTAKE_SOUND_PATH = SND_PATH + "mistake.wav";
 
-	private AudioInputStream audioInputStream;
-	private SourceDataLine line;
-	private byte[] abData;
-	private int	nBytesRead;
-	private int nStreamLengthInBytes;
+	private File soundFile;
+	private Clip sound;
 
 	/**
-	 * Utwórz nowy dźwięk do otwarzania.
+	 * Utwórz nowy dźwięk do odtwarzania.
 	 *
 	 * @param soundId Identyfikator dźwięku. Do wyboru są:<br>
-	 *                <code>0</code> - pisanie na maszynie<br>
-	 *                <code>1</code> - dźwięk błędu
+	 *                <code>0</code> - naciśnięcie klawisza na maszynie
+	 *                                 do pisania<br>
+	 *                <code>1</code> - błędne przepisanie znaku
 	 */
 	public Sound(int soundId) {
+		if (soundId == 0) {
+			soundFile = new File(TYPING_SOUND_PATH);
+			initSound();
+		} else {
+			soundFile = new File(MISTAKE_SOUND_PATH);
+		}
+	}
+
+	private void initSound() {
 		try {
-			InputStream fileInputStream;
-			if (soundId == 0) {
-				fileInputStream = new FileInputStream(TYPING_SOUND_FILENAME);
+			AudioInputStream audioInputStream =
+				AudioSystem.getAudioInputStream(soundFile);
+			if (audioInputStream != null) {
+				AudioFormat	format = audioInputStream.getFormat();
+				DataLine.Info info = new DataLine.Info(Clip.class, format);
+				sound = (Clip) AudioSystem.getLine(info);
+				sound.open(audioInputStream);
 			} else {
-				fileInputStream = new FileInputStream(MISTAKE_SOUND_FILENAME);
+				System.err.println("ClipPlayer: can't get data from file "
+						+ soundFile.getName());
 			}
-			InputStream inputStream = new BufferedInputStream(fileInputStream);
-
-			audioInputStream = AudioSystem.getAudioInputStream(inputStream);
-			if (!audioInputStream.markSupported()) {
-				System.err.println("CoolKey: Sound: resetting not supported");
-				System.exit(1);
-			}
-			AudioFormat	audioFormat = audioInputStream.getFormat();
-
-			DataLine.Info info = new DataLine.Info(SourceDataLine.class,
-					audioFormat);
-			line = (SourceDataLine) AudioSystem.getLine(info);
-			line.open(audioFormat);
-			abData = new byte[EXTERNAL_BUFFER_SIZE];
-			nBytesRead = 0;
-			if (audioInputStream.getFrameLength() == AudioSystem.NOT_SPECIFIED ||
-					audioFormat.getFrameSize() == AudioSystem.NOT_SPECIFIED) {
-				System.out.println("CoolKey: Sound: cannot calculate length of"
-						+ "AudioInputStream!");
-				System.exit(1);
-			}
-			long lStreamLengthInBytes = audioInputStream.getFrameLength()
-					* audioFormat.getFrameSize();
-			if (lStreamLengthInBytes > Integer.MAX_VALUE) {
-				System.err.println("CoolKey: Sound: length of AudioInputStream"
-						+ "exceeds 2^31, cannot properly reset stream!");
-				System.exit(1);
-			}
-			nStreamLengthInBytes = (int) lStreamLengthInBytes;
-
-			line.start();
-
 		} catch (UnsupportedAudioFileException e) {
 			e.printStackTrace();
-		} catch (FileNotFoundException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (LineUnavailableException e) {
 			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
 	}
 
+	/**
+	 * Odtwarza dźwięk.
+	 */
 	public void play() {
-		try {
-			audioInputStream.mark(nStreamLengthInBytes);
-			nBytesRead = 0;
-			while (nBytesRead != -1) {
-				nBytesRead = audioInputStream.read(abData, 0, abData.length);
-				if (nBytesRead >= 0) {
-					line.write(abData, 0, nBytesRead);
-				}
-			}
-			audioInputStream.reset();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void close() {
-		line.drain();
-		line.close();		
+		initSound();
+		sound.loop(0);
 	}
 }
