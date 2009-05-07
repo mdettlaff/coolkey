1519b3a803ef4b33333d23a3ba907bb444bd877b
wundzunhome@wp.pl <wundzunhome@wp.pl@c213c24a-fd2c-11dd-a7be-4b3ef3b0700c>
2009-05-07 14:43:27 +0000

    Klawiatura ekranowa: możliwość wyboru układu klawiatury w ustawieniach (QWERTY lub Dvorak), podświetlanie znaków specjalnych i cyfr.

--- trunk/src/coolkey/Config.java
+++ trunk/src/coolkey/Config.java
@@ -6,6 +6,9 @@ import java.io.Serializable;
  * Ustawienia programu wybrane przez użytkownika.
  */
 public class Config implements Serializable {
+	public static final int QWERTY = 0;
+	public static final int DVORAK = 1;
+
 	private static final long serialVersionUID = 1L;
 
 	private boolean soundOn;
@@ -13,6 +16,7 @@ public class Config implements Serializable {
 	private boolean showGraphs;
 	private boolean continueAtMistakes;
 	private String lineBreakers;
+	private int keyboardLayout;
 
 	public Config() {
 		soundOn = true;
@@ -20,6 +24,7 @@ public class Config implements Serializable {
 		showGraphs = true;
 		continueAtMistakes = true;
 		lineBreakers = "\n";
+		keyboardLayout = QWERTY;
 	}
 
 	public boolean isSoundOn() {
@@ -60,5 +65,13 @@ public class Config implements Serializable {
 
 	public void setLineBreakers(String lineBreakers) {
 		this.lineBreakers = lineBreakers;
+	}
+
+	public int getKeyboardLayout() {
+		return keyboardLayout;
+	}
+
+	public void setKeyboardLayout(int keyboardLayout) {
+		this.keyboardLayout = keyboardLayout;
 	}
 }
--- trunk/src/coolkey/gui/Keyboard.java
+++ trunk/src/coolkey/gui/Keyboard.java
@@ -11,6 +11,7 @@ import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Canvas;
 
+import coolkey.Config;
 import coolkey.CoolKey;
 
 /**
@@ -22,22 +23,32 @@ public class Keyboard {
 	private final int KEY_HEIGHT = KEY_WIDTH;
 	private final int KEY_X_PADDING = 8;
 	private final int KEY_Y_PADDING = 5;
-	private final int X_MARGIN = 56; // pozycja lewego górnego klawisza
-	private final int Y_MARGIN = 37; // pozycja lewego górnego klawisza
-	private final int[] ROWS_X_MARGINS = { X_MARGIN, X_MARGIN + 9,
-			X_MARGIN + 28 };
-	private final int[] ROWS_Y_MARGINS = { Y_MARGIN, Y_MARGIN + KEY_HEIGHT,
-			Y_MARGIN + KEY_HEIGHT * 2 };
+	private final int KEY_Y_TOP_PADDING = 3;
+	private final int KEY_Y_BOTTOM_PADDING = 18;
+	private final Point MARGIN = new Point(1, 0);
+	private final int[] ROWS_X_MARGINS = { MARGIN.x, MARGIN.x + 55,
+			MARGIN.x + 64, MARGIN.x + 83 };
+	private final int[] ROWS_Y_MARGINS = { MARGIN.y, MARGIN.y + KEY_HEIGHT,
+			MARGIN.y + KEY_HEIGHT * 2, MARGIN.y + KEY_HEIGHT * 3 };
 	private final Point LEFT_SHIFT_COORDS = new Point(0, 111);
 	private final Point RIGHT_SHIFT_COORDS = new Point(453, 111);
 	private final Point SPACE_COORDS = new Point(148, 148);
 	private final Point ENTER_COORDS = new Point(471, 74);
 	private final Point ALT_GR_COORDS = new Point(370, 148);
-	private final String[] QWERTY = { "qwertyuiop",
-									  "asdfghjkl;'",
-									  "zxcvbnm,./" };
+	private final String[] QWERTY = {
+	        "~`!1@2#3$4%5^6&7*8(9)0_-+=",
+	        "qwertyuiop{[}]|\\",
+	        "asdfghjkl:;\"'",
+	        "zxcvbnm<,>.?/"
+	};
+	private final String[] Dvorak = {
+	        "~`!1@2#3$4%5^6&7*8(9)0{[}]",
+	        "\"'<,>.pyfgcrl?/+=|\\",
+	            "aoeuidhtns_-",
+	           ":;qjkxbmwvz"
+	};
 	private final String POLISH_CHARS = "ĄąĆćĘęŁłŃńÓóŚśŻżŹź";
-	private final String LATIN_CHARS = "AaCcEeLlNnOoSsZzZz";
+	private final String POLISH2LATIN_CHARS = "AaCcEeLlNnOoSsZzZz";
 	private final Canvas canvas;
 	private final Image blankKeyboard;
 	private final Image keyHighlighted;
@@ -52,6 +63,8 @@ public class Keyboard {
 	private final Image spaceMistake;
 	private final Image altGrHighlighted;
 	private final Image altGrMistake;
+	private final Image keyLongHighlighted;
+	private final Image keyLongMistake;
 
 	private Character nextChar;
 	private boolean leftHand;
@@ -87,6 +100,10 @@ public class Keyboard {
 				+ "images" + File.separator + "altgr_highlighted.png");
 		altGrMistake = new Image(GUI.display, "data" + File.separator
 				+ "images" + File.separator + "altgr_mistake.png");
+		keyLongHighlighted = new Image(GUI.display, "data" + File.separator
+				+ "images" + File.separator + "key_long_highlighted.png");
+		keyLongMistake = new Image(GUI.display, "data" + File.separator
+				+ "images" + File.separator + "key_long_mistake.png");
 
 		canvas.addPaintListener(new PaintListener() {
 			public void paintControl(PaintEvent pe) {
@@ -96,31 +113,69 @@ public class Keyboard {
 				gc.setForeground(GUI.display.getSystemColor(SWT.COLOR_BLACK));
 				gc.fillRectangle(0, 0, canvasSize.x, canvasSize.y); // tło
 				gc.drawImage(blankKeyboard, 0, 0);
-				// podświetl litery z układu klawiatury
-				String[] layout = QWERTY;
+				String[] layout;
+				if (CoolKey.getUser().getConfig().getKeyboardLayout()
+						== Config.QWERTY) {
+					layout = QWERTY;
+				} else {
+					layout = Dvorak;
+				}
+				boolean isShiftCharacter = false;
+				// podświetl zwykłe klawisze z układu klawiatury
 				for (int j=0; j < layout.length; j++) {
+					int keyFromLeft = 0; // który z kolei klawisz od lewej
+					int x = ROWS_X_MARGINS[j];
 					for (int i=0; i < layout[j].length(); i++) {
 						char c = layout[j].charAt(i);
-						char keyChar; // klawisz odpowiadający znakowi nextChar
-						if (POLISH_CHARS.indexOf(nextChar) != -1) {
-							keyChar = LATIN_CHARS.charAt(
-									POLISH_CHARS.indexOf(nextChar));
-						} else {
-							keyChar = nextChar;
-						}
-						int x = ROWS_X_MARGINS[j] + KEY_WIDTH * i;
+						char keyChar = nextChar;
 						int y = ROWS_Y_MARGINS[j];
-						if (c == keyChar
-								|| Character.toUpperCase(c) == keyChar) {
-							if (CoolKey.getCurrentLesson().isMistakeMade()) {
-								gc.drawImage(keyMistake, x, y);
-							} else {
-								gc.drawImage(keyHighlighted, x, y);
+						if (Character.isLetter(c)) { // podświetlanie liter
+							if (POLISH_CHARS.indexOf(nextChar) != -1) {
+								keyChar = POLISH2LATIN_CHARS.charAt(
+										POLISH_CHARS.indexOf(nextChar));
 							}
-							leftHand = i < 5;
+							if (c == keyChar
+									|| Character.toUpperCase(c) == keyChar) {
+								if (CoolKey.getCurrentLesson().isMistakeMade()) {
+									gc.drawImage(keyMistake, x, y);
+								} else {
+									gc.drawImage(keyHighlighted, x, y);
+								}
+								leftHand = j > 0 ? keyFromLeft < 5
+										: keyFromLeft < 7;
+								isShiftCharacter = Character.isUpperCase(nextChar);
+							}
+							gc.drawString("" + Character.toUpperCase(c),
+									x + KEY_X_PADDING, y + KEY_Y_PADDING, true);
+						} else { // podświetlanie znaków specjalnych
+							if (c == keyChar
+									|| layout[j].charAt(i+1) == keyChar) {
+								if (c == '|') {
+									if (CoolKey.getCurrentLesson().isMistakeMade()) {
+										gc.drawImage(keyLongMistake, x, y);
+									} else {
+										gc.drawImage(keyLongHighlighted, x, y);
+									}
+								} else {
+									if (CoolKey.getCurrentLesson().isMistakeMade()) {
+										gc.drawImage(keyMistake, x, y);
+									} else {
+										gc.drawImage(keyHighlighted, x, y);
+									}
+								}
+								leftHand = j > 0 ? keyFromLeft < 5
+										: keyFromLeft < 7;
+								isShiftCharacter = c == keyChar;
+							}
+							gc.drawString("" + c, x + KEY_X_PADDING,
+									y + KEY_Y_TOP_PADDING, true);
+							i++;
+							c = layout[j].charAt(i);
+							gc.drawString("" + c, x + KEY_X_PADDING,
+									y + KEY_Y_BOTTOM_PADDING, true);
 						}
-						gc.drawString("" + Character.toUpperCase(c),
-								x + KEY_X_PADDING, y + KEY_Y_PADDING, true);
+						x += KEY_WIDTH;
+						keyFromLeft++;
 					}
 				}
 				// podświetl spację
@@ -140,7 +195,7 @@ public class Keyboard {
 					}
 				}
 				// podświetl Shift
-				if (Character.isUpperCase(nextChar)) {
+				if (isShiftCharacter) {
 					if (CoolKey.getCurrentLesson().isMistakeMade()) {
 						if (leftHand) {
 							gc.drawImage(rightShiftMistake,
