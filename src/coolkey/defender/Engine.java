43cb807e6404a9b71ed2fd4c1522d6dd4c9cf9c5
hybris@o2.pl <hybris@o2.pl@c213c24a-fd2c-11dd-a7be-4b3ef3b0700c>
2009-05-18 00:02:23 +0000

    Defender rc2 - dodalem komentarze

--- src/coolkey/defender/Engine.java
+++ src/coolkey/defender/Engine.java
@@ -16,58 +16,166 @@ import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Display;
 
 import coolkey.CoolKey;
-
+/**
+ * Klasa mechanizmu animacji gry.
+ */
 public class Engine implements Runnable {
+	/**
+	 * Stała zdefiniowana jako stan wyświetlenia menu gry.
+	 */
 	public static final int STATE_MENU = 0;
+	/**
+	 * Stała zdefiniowana jako stan wyświetlenia gry.
+	 */
 	public static final int STATE_GAME = 1;
+	/**
+	 * Stała zdefiniowana jako stan wyświetlenia wyniku gry.
+	 */
 	public static final int STATE_RESULT = 2;
+	/**
+	 * Stała zdefiniowana jako stan wyświetlenia listy najlepszych wyników.
+	 */
 	public static final int STATE_TOP10 = 3;
+	/**
+	 * Stała zdefiniowana jako stan wyświetlenia pomocy.
+	 */
 	public static final int STATE_HELP = 4;
-	
+	/**
+	 * Stała zdefiniowana jako identyfikator przycisku 'Kontynuj'
+	 */
 	public static final int MENU_CONTINUE = 0;
+	/**
+	 * Stała zdefiniowana jako identyfikator przycisku 'Nowa gra'
+	 */
 	public static final int MENU_NEW = 1;
+	/**
+	 * Stała zdefiniowana jako identyfikator przycisku 'Top 10'
+	 */
 	public static final int MENU_TOP10 = 2;
+	/**
+	 * Stała zdefiniowana jako identyfikator przycisku 'Pomoc'
+	 */
 	public static final int MENU_HELP = 3;
+	/**
+	 * Stała zdefiniowana jako identyfikator przycisku powrotu do menu.
+	 */
 	public static final int MENU_ESC = 4;
-	
+	/**
+	 * Stała zdefiniowana jako szerokość obszaru gry.
+	 */
 	public static final int WIDTH = 640;
+	/**
+	 * Stała zdefiniowana jako wysokość obszaru gry.
+	 */
 	public static final int HEIGHT = 480;
-	
+	/**
+	 * Stała zdefiniowana jako maksymalna liczba żyć.
+	 */
 	public static final int GAME_LIFE_MAX = 5;
+	/**
+	 * Stała zdefiniowana jako maksymalna liczba poziomów.
+	 */
 	public static final int GAME_LEVEL_MAX = 21;
+	/**
+	 * Stała zdefiniowana jako granica po której eksploduje bomba.
+	 */
 	public static final int GAME_GROUND = 416;
+	/**
+	 * Stała zdefiniowana jako szybkość animacji wybuchu.
+	 */
 	public static final double GAME_EXPLOSION_SPEED = 10.0;
-	
-	public static final int TOP10_RESULT = 10;
-	
+	/**
+	 * Stała zdefiniowana jako maksymalna liczba wyników w 'Top 10'
+	 */
+	public static final int TOP10_RESULTS = 10;
+	/**
+	 * Stała zdefiniowana jako czas przerwy w animacji.
+	 */
 	private final int INTERVAL = 40;
-	
+	/**
+	 * Uchwyt do obszaru, na którym jest rysowana gra.
+	 */
 	private final Canvas container;
+	/**
+	 * Uchwyt do urządzenia, które wyświetla obraz.
+	 */
 	private final Display display;
-	
+	/**
+	 * Liczba klatek na sekunte.
+	 */
 	private double fps;
+	/**
+	 * Czy pokazać liczbę klatek na sekunde.
+	 */
 	private boolean showFps;
-	
+	/**
+	 * Służy do ustalenia liczby klatek na sekunde.
+	 */
 	private long timeLast;
+	/**
+	 * Zlicza ilość klatek.
+	 */
 	private int countFrame;
-	
+	/**
+	 * Czy jest nową gra.
+	 */
 	private boolean newGame;
+	/**
+	 * Status gry.
+	 */
 	private int state;
-	
+	/**
+	 * Identyfikator przycisku, który jest zaznaczony.
+	 */
 	private int menuSelectId;
+	/**
+	 * Czy przycisk powrotu do menu jest zaznaczony.
+	 */
 	private boolean menuEscSelect;
+	/**
+	 * Lista rozmiarów i pozycji poszczególnych przycisków.
+	 */
 	private List<Rectangle> menu;
-	
+	/**
+	 * Poziom aktualnej gry.
+	 */
 	private int gameLevel;
+	/**
+	 * Liczba żyć aktualnej gry.
+	 */
 	private int gameLife;
+	/**
+	 * Liczba punktów aktualnej gry.
+	 */
 	private int gameScore;
+	/**
+	 * Czas trwania aktualnej gry.
+	 */
 	private long gameTime;
+	/**
+	 * Aktualnie wpisane słowo.
+	 */
 	private String gameWord;
+	/**
+	 * Czy słowo ma być wyczyszczone.
+	 */
 	private boolean gameWordClear;
+	/**
+	 * Mapa wszystkich bomb.
+	 */
 	private Map<String, Bomb> gameBombs;
+	/**
+	 * Lista bomb, które eksplodowały.
+	 */
 	private List<Bomb> gameBombsExplosion;
+	/**
+	 * Uchwyt do stopera.
+	 */
 	private final Timer gameTimer;
-	
+	/**
+	 * Tworzy nowy obiekt mechanizmu gry.
+	 * @param container obszar na którym ma być rysowana gra.
+	 */
 	public Engine(Canvas container) {
 		this.container = container;
 		this.display = this.container.getDisplay();
@@ -86,43 +194,66 @@ public class Engine implements Runnable {
 		this.gameTimer = new Timer("gameTimer");
 		this.gameNew();
 	}
-	
+	/**
+	 * Zwraca liczbę klatek na sekunde.
+	 * @return liczba klatek na sekunde
+	 */
 	public synchronized double getFps() {
 		return this.fps;
 	}
-
+	/**
+	 * Czy liczba klatek na sekunde ma być wyświetlona na ekranie.
+	 * @return true - wyświetl, false - nie wyświetlaj
+	 */
 	public synchronized boolean showFps() {
 		return this.showFps;
 	}
-	
+	/**
+	 * Ustawia czy liczba klatek na sekunde ma być wyświetlona. 
+	 * @param showFps true - wyświetl, false - nie wyświetlaj
+	 */
 	public synchronized void showFps(boolean showFps) {
 		this.showFps = showFps;
 	}
-	
+	/**
+	 * Zwraca status gry.
+	 * @return status gry
+	 */
 	public synchronized int getState() {
 		return this.state;
 	}
-	
+	/**
+	 * Sprawdza czy obecnie jest nową grą.
+	 * @return true - nowa gra, false w przeciwnym wypadku
+	 */
 	public synchronized boolean isNewGame() {
 		return this.newGame;
 	}
-	
+	/**
+	 * Zwraca identyfikator zaznaczonego przycisku.
+	 * @return identyfikator przycisku
+	 */
 	public synchronized int getMenuSelectId() {
 		return this.menuSelectId;
 	}
-	
-	public synchronized boolean getMenuEscSelect() {
+	/**
+	 * Sprawdza czy przycisk powrotu do menu jest zaznaczony.
+	 * @return true gdy jest zaznaczony, false w przeciwnym wypadku
+	 */
+	public synchronized boolean isMenuEscSelect() {
 		return this.menuEscSelect;
 	}
-	
-	public synchronized List<Rectangle> getMenu() {
-		return this.menu;
-	}
-	
+	/**
+	 * Zwraca wymiary i pozycję przycisko o podanym identyfikatorze.
+	 * @param id identyfikator przycisku
+	 * @return wymiary i pozycja przycisku
+	 */
 	public synchronized Rectangle getMenu(int id) {
 		return this.menu.get(id);
 	}
-	
+	/**
+	 * Zmienia identyfikator zaznaczonego przycisku na następny.
+	 */
 	public synchronized void keyNext() {
 		switch(this.menuSelectId) {
 			case MENU_CONTINUE:
@@ -137,7 +268,9 @@ public class Engine implements Runnable {
 		}
 		this.menuSelectId++;
 	}
-	
+	/**
+	 * Zmienia identyfikator zaznaczonego przycisku na poprzedni.
+	 */
 	public synchronized void keyPrev() {
 		switch(this.menuSelectId) {
 			case MENU_CONTINUE:
@@ -157,7 +290,9 @@ public class Engine implements Runnable {
 		}
 		this.menuSelectId--;
 	}
-	
+	/**
+	 * Zmienia status gry odpowiednio dla zaznaczonego identyfikatora przycisku
+	 */
 	public synchronized void keyEnter() {
 		switch(this.menuSelectId) {
 			case MENU_CONTINUE:
@@ -177,7 +312,9 @@ public class Engine implements Runnable {
 				break;
 		}
 	}
-	
+	/**
+	 * Zmenia status gry na powrót do menu.
+	 */
 	public synchronized void keyEsc() {
 		switch(this.state) {
 		case STATE_GAME:
@@ -192,7 +329,11 @@ public class Engine implements Runnable {
 		}
 		this.state = STATE_MENU;
 	}
-	
+	/**
+	 * Zmienia status gry odpowiednio dla kliknięcia lewego przycisku myszy.
+	 * @param x współrzędna kursora na osi x
+	 * @param y współrzędna kursora na osi y
+	 */
 	public synchronized void mouseUp(int x, int y) {
 		switch(this.state) {
 			case STATE_MENU:
@@ -235,7 +376,19 @@ public class Engine implements Runnable {
 				break;
 		}
 	}
-	
+	/**
+	 * Zmienia status gry na powrót do menu dla kliknięcia prawego przycisku myszy.
+	 */
+	public synchronized void mouseUpEsc() {
+		if(this.state == STATE_RESULT)
+			this.menuSelectId = MENU_NEW;
+		this.state = STATE_MENU;
+	}
+	/**
+	 * Zmienia idetyfikator zaznaczonego przycisku odpowiednio dla podanych współrzędnych kursora
+	 * @param x współrzędna kursora na osi x
+	 * @param y współrzędna kursora na osi y
+	 */
 	public synchronized void mouseMove(int x, int y) {
 		switch(this.state) {
 			case STATE_MENU:
@@ -271,51 +424,87 @@ public class Engine implements Runnable {
 				break;
 		}
 	}
-	
+	/**
+	 * Zwraca liczbę aktualnych żyć.
+	 * @return liczbę aktualnych żyć
+	 */
 	public synchronized int gameGetLife() {
 		return this.gameLife;
 	}
-	
+	/**
+	 * Zwraca aktualny poziom gry.
+	 * @return poziom gry
+	 */
 	public synchronized int gameGetLevel() {
 		return this.gameLevel;
 	}
-	
+	/**
+	 * Zwraca aktualną liczbę zdobytych punktów.
+	 * @return liczba punktów
+	 */
 	public synchronized int gameGetScore() {
 		return this.gameScore;
 	}
-	
+	/**
+	 * Zwraca aktulny czas trwania gry.
+	 * @return czas trwania gry
+	 */
 	public synchronized long gameGetTime() {
 		return this.gameTime;
 	}
-	
+	/**
+	 * Zwraca aktualnie wpisane słowo.
+	 * @return wpisane słowo
+	 */
 	public synchronized String gameGetWord() {
 		return this.gameWord;
 	}
-	
-	public synchronized boolean gameGetWordClear() {
+	/**
+	 * Sprawdza czy słowo jest wyczyszczone.
+	 * @return true gdy słowo jest wyczyszczone, false w przeciwnym wypadku
+	 */
+	public synchronized boolean gameIsWordClear() {
 		return this.gameWordClear;
 	}
-	
+	/**
+	 * Zwraca kolekcje wszystich bomb.
+	 * @return kolekcja bomb
+	 */
 	public synchronized Collection<Bomb> gameGetBombs() {
 		return this.gameBombs.values();
 	}
-	
+	/**
+	 * Zwraca list bomb, które eksplodowały.
+	 * @return lista eksplodujących bomb
+	 */
 	public synchronized List<Bomb> gameGetBombsExplosion() {
 		return this.gameBombsExplosion;
 	}
-	
+	/**
+	 * Zwraca maksymalną liczbę bomb, które mogą być w grze w tym samym czasie. Zależy od poziomu gry. 
+	 * @return maksymalną liczbę bomb
+	 */
 	private synchronized int gameHowBombs() {
 		return this.gameLevel + 2;
 	}
-	
+	/**
+	 * Zwraca jak szybko spada bomba odpowiednio dla poziomu gry. 
+	 * @return szydkość spadania bomby
+	 */
 	private double gameHowFast() {
 		return this.gameLevel * 1.5 + 14.0;
 	}
-	
+	/**
+	 * Zwraca liczbę punktów, które trzeba zdobyć by przejść na kolejny poziom gry.
+	 * @return liczbę punktów, które trzeba zdobyć
+	 */
 	private synchronized int gameHowScore() {
 		return (int)Math.pow(this.gameLevel, 2.0) + 19 * this.gameLevel - 5;
 	}
-	
+	/**
+	 * Dodaje do końca słowa podany znak.
+	 * @param c znak, który zostanie dodany
+	 */
 	public synchronized void gameWordAdd(char c) {
 		String specialChar = ",./;\'[]<>?:\"{}!@#$%^&*()_+|\\-=";
 		if(Character.isLetterOrDigit(c) || specialChar.indexOf(c) != -1) {
@@ -327,7 +516,9 @@ public class Engine implements Runnable {
 		}
 		this.gameWordEnter();
 	}
-	
+	/**
+	 * Usuwa ostatni znak ze słowa.
+	 */
 	public synchronized void gameWordDel() {
 		if(this.gameWordClear) {
 			this.gameWord = "";
@@ -337,7 +528,9 @@ public class Engine implements Runnable {
 			this.gameWord = this.gameWord.substring(0, this.gameWord.length() - 1);
 		this.gameWordEnter();
 	}
-	
+	/**
+	 * Usuwa bombę, która ma etykietę taką samą jak wpisane słowo oraz zwiększa liczbę punktów o 1.
+	 */
 	private synchronized void gameWordEnter() {
 		if(this.gameBombs.containsKey(this.gameWord)) {
 			this.gameBombsExplosion.add(this.gameBombs.get(this.gameWord));
@@ -347,7 +540,9 @@ public class Engine implements Runnable {
 			this.gameWordClear = true;
 		}
 	}
-	
+	/**
+	 * Ustawia wszystkie parametry gry na początkowe.
+	 */
 	private synchronized void gameNew() {
 		this.gameLife = GAME_LIFE_MAX;
 		this.gameScore = 0;
@@ -358,7 +553,9 @@ public class Engine implements Runnable {
 		this.gameBombs = new HashMap<String, Bomb>();
 		this.gameBombsExplosion = new ArrayList<Bomb>();
 	}
-	
+	/**
+	 * Uzupełnia liczbę bomb do maksymalnej ilości, które mogą być w jednej chwili.
+	 */
 	private synchronized void gameCreateBombs() {
 		Thread gameCreateBombsThread = new Thread(new Runnable() {
 			public void run() {
@@ -381,7 +578,9 @@ public class Engine implements Runnable {
 		}, "gameCreateBombs");
 		gameCreateBombsThread.start();
 	}
-	
+	/**
+	 * Odgrywa dźwięk eksplozji bomby.
+	 */
 	private synchronized void gameSoundExplosion() {
 		Thread gameSoundExplosionThread = new Thread(new Runnable() {
 			public void run() {
@@ -391,7 +590,9 @@ public class Engine implements Runnable {
 		}, "gameSoundExplosion");
 		gameSoundExplosionThread.start();
 	}
-	
+	/**
+	 * Odgrywa dźwięk zniszcenia bomby.
+	 */
 	private synchronized void gameSoundExplosion2() {
 		Thread gameSoundExplosionThread = new Thread(new Runnable() {
 			public void run() {
@@ -401,33 +602,38 @@ public class Engine implements Runnable {
 		}, "gameSoundExplosion2");
 		gameSoundExplosionThread.start();
 	}
-	
-	private synchronized void top10ScoreAdd() {
+	/**
+	 * Dodaje wynik gry do listy najlepszych wyników.
+	 * @param score liczba zdobytych punktów
+	 * @param time czas trwania gry
+	 * @param level poziom na którym gracz skończył grę
+	 */
+	private synchronized void top10ScoreAdd(final int score, final long time, final int level) {
 		Thread top10ScoreAddThread = new Thread(new Runnable() {
 			public void run() {
 				List<Score> highscore = CoolKey.getUser().getHighscore();
-				for(int i = 0; i < TOP10_RESULT; i++) {
-					Score score = null;
+				for(int i = 0; i < TOP10_RESULTS; i++) {
+					Score scoreObj = null;
 					try {
-						score = highscore.get(i);
+						scoreObj = highscore.get(i);
 					} catch(IndexOutOfBoundsException e) {
-						highscore.add(i, new Score(gameScore, gameTime, gameLevel));
+						highscore.add(i, new Score(score, time, level));
 						CoolKey.getUser().setHighscore(highscore);
 						CoolKey.persistState();
 						return;
 					}
-					if(score.getScore() < gameScore) {
-						highscore.set(i, new Score(gameScore, gameTime, gameLevel));
-						Score scoreTmp = null;
-						for(int j = i + 1; j < TOP10_RESULT; j++) {
+					if(scoreObj.getScore() < gameScore) {
+						highscore.set(i, new Score(score, time, level));
+						Score scoreObjTmp = null;
+						for(int j = i + 1; j < TOP10_RESULTS; j++) {
 							try {
-								scoreTmp = highscore.get(j);
+								scoreObjTmp = highscore.get(j);
 							} catch(IndexOutOfBoundsException e) {
-								highscore.add(j, score);
+								highscore.add(j, scoreObj);
 								break;
 							}
-							highscore.set(j, score);
-							score = scoreTmp;
+							highscore.set(j, scoreObj);
+							scoreObj = scoreObjTmp;
 						}
 						CoolKey.getUser().setHighscore(highscore);
 						CoolKey.persistState();
@@ -438,7 +644,9 @@ public class Engine implements Runnable {
 		}, "top10ScoreAdd");
 		top10ScoreAddThread.start();
 	}
-	
+	/**
+	 * Przelicza kolejną klatkę animacji.
+	 */
 	private synchronized void action() {
 		switch(this.state) {
 			case STATE_GAME:
@@ -463,14 +671,14 @@ public class Engine implements Runnable {
 				}
 				this.gameCreateBombs();
 				if(this.gameLife < 1) {
-					this.top10ScoreAdd();
+					this.top10ScoreAdd(this.gameScore, this.gameTime, this.gameLevel);
 					this.newGame = true;
 					this.state = STATE_RESULT;
 				}
 				break;
 		}
 	}
-	
+
 	public synchronized void run() {
 		long timeStart = System.currentTimeMillis();
 		int time = (int)(timeStart - this.timeLast);
@@ -490,7 +698,9 @@ public class Engine implements Runnable {
 		time = INTERVAL - (int)(System.currentTimeMillis() - timeStart);
 		this.display.timerExec((time < 0 ? 0 : time), this);
 	}
-	
+	/**
+	 * Uruchamia mechanizm animacji gry.
+	 */
 	public synchronized void start() {
 		this.gameTimer.schedule(new TimerTask () {
 			private long timeBefor = System.currentTimeMillis();
@@ -506,6 +716,9 @@ public class Engine implements Runnable {
 		this.timeLast = System.currentTimeMillis();
 		this.display.timerExec(0, this);
 	}
+	/**
+	 * Zatrzymuję mechanizm animacji gry.
+	 */
 	public synchronized void stop() {
 		this.gameTimer.cancel();
 		if(!this.display.isDisposed())
--- src/coolkey/defender/MouseMove.java
+++ src/coolkey/defender/MouseMove.java
@@ -3,9 +3,18 @@ package coolkey.defender;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.MouseMoveListener;
 
+/**
+ * Klasa obsługująca zdarzenie przesunięcia myszki.
+ */
 public class MouseMove implements MouseMoveListener {
+	/**
+	 * Uchwyt do mechanizmu gry.
+	 */
 	private Engine engine;
-	
+	/**
+	 * Tworzy nowy obiekt obsługujący zdarzenie przesunięcia myszki.
+	 * @param engine uchwyt do mechanizmu gry
+	 */
 	public MouseMove(Engine engine) {
 		this.engine = engine;
 	}
